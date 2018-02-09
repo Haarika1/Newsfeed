6e8458f180b67d5073b1bc1b1d1be8997461e35b
Haarika1 <harika.vallabhani1@gmail.com>
2018-02-09 05:33:30 -0800

    register

---
+++ src/register/register.controller.js
@@ -0,0 +1,16 @@
+class RegisterController{
+    constructor()
+    {
+        this.text="";
+        this.email="";
+        this.password="";
+        this.password="";
+    }
+    doRegister(){
+        console.log(this.text);
+        console.log(this.email);
+        console.log(this.password);
+        console.log(this.password);
+    }
+}
+export default RegisterController;
